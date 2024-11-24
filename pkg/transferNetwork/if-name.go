179ae0126af0212a091dc57d36403da8131ff394
TimeForANinja <t.kutscha@yahoo.de>
2024-11-24 09:49:13 +0100

    add func to split wg name + switch to 3 letters for indexes

--- pkg/transferNetwork/if-name.go
+++ pkg/transferNetwork/if-name.go
@@ -1,9 +1,32 @@
 package transferNetwork
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func BuildWireguardInterfaceName(selfIDX uint8, peerIDX uint8) string {
-	// TODO: define a "max" idx and add some check for it
-	// if we just keep uint8 we should change is to 3d
-	return fmt.Sprintf("wg1%02d%02d", selfIDX, peerIDX)
+	// 3 digits since uint8 has a max value of 255
+	return fmt.Sprintf("wg1%03d%03d", selfIDX, peerIDX)
+}
+
+func SplitWireguardInterfaceName(name string) (uint8, uint8, error) {
+	if len(name) != 11 || name[:3] != "wg1" {
+		return 0, 0, fmt.Errorf("invalid interface name format")
+	}
+
+	selfIDXPart := name[3:6]
+	peerIDXPart := name[6:9]
+
+	selfIDX, err := strconv.Atoi(selfIDXPart)
+	if err != nil {
+		return 0, 0, fmt.Errorf("error parsing selfIDX: %w", err)
+	}
+
+	peerIDX, err := strconv.Atoi(peerIDXPart)
+	if err != nil {
+		return 0, 0, fmt.Errorf("error parsing peerIDX: %w", err)
+	}
+
+	return uint8(selfIDX), uint8(peerIDX), nil
 }
